20582cffd785875cfe78482dd878507892ce3cd2
Jyothi Samanthula <122471924+Jyothisamanthula@users.noreply.github.com>
2024-06-06 15:19:11 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://65.0.45.167:9000/',
+  serverUrl: 'http://65.1.106.129:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
